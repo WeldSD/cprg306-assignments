a2bf81da3496b0c432ee17b0ae1e4068c2b12afc
Meron <119772197+WeldSD@users.noreply.github.com>
2024-02-07 05:48:43 -0700

    Update page.js
    padding

--- app/page.js
+++ app/page.js
@@ -2,16 +2,16 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <main className="bg-black">
-      <h1 className="text-green-500">WebDev2</h1>
+    <main className="bg-black ml-6" >
+      <h1 className="text-green-500 text-xl mt-4 mb-4">WebDev2</h1>
 
       <ul>
-        <li className="bg-black text-green-500">
+        <li className="bg-black text-lg text-green-500 mb-4">
           <Link href="week-2">Week 2</Link>
         </li>
       </ul>
       <ul>
-        <li className="bg-black text-green-500">
+        <li className="bg-black text-lg text-green-500 mb-4">
           <Link href="week-3">Week 3</Link>
         </li>
       </ul>
