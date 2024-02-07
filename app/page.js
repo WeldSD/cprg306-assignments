c29d08a33dbd2c1e78986e1d2750e3021ede8d86
Meron <119772197+WeldSD@users.noreply.github.com>
2024-02-06 23:57:37 -0700

    Main Page

--- app/page.js
+++ app/page.js
@@ -10,6 +10,12 @@ export default function Page() {
           <Link href="week-2">Week 2</Link>
         </li>
       </ul>
+      <ul>
+        <li>
+          <Link href="week-3">Week 3</Link>
+        </li>
+      </ul>
+      
 
     </main>
   );
